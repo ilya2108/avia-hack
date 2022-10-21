021e52459e2f8e103bd436d6396eab7c955192f2
Yaroslav <yar21sav@gmail.com>
2022-10-22 02:05:12 +0300

    initial commit

---
+++ docker/README.md
@@ -0,0 +1,8 @@
+# Docker scripts
+
+Install and run Docker container:
+
+```bash
+./docker/install.sh
+./run_service.sh
+```
---
+++ docker/run_service.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+EXEC_PATH=$PWD
+
+cd $ROOT_DIR
+docker compose  -f $ROOT_DIR/docker/docker-compose.yml up -d
+                                
+
+
+cd $EXEC_PATH
